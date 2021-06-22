f77f9f6aaf2212598cd0bf980eb90ad29afab93a
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2021-06-22 13:21:55 +0300

    basic update cycle

---
+++ src/components/Upgrade.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <button @click="connectSerial">1. Connect Serial</button>
+    <button @click="gotoDFU">2. Go to DFU</button>
+    <button @click="connectDFU">3. Connect DFU</button>
+    <button @click="writeFirmware">4. Write firmware</button>
+    <p>
+      {{ status }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { WebDFU } from "dfu"
+
+export default {
+  name: 'Upgrade',
+  data() {
+    return {
+      status: 'Connect Flipper',
+      port: undefined,
+      webdfu: undefined,
+      firmwareFile: undefined
+    }
+  },
+  methods : {
+    async connectSerial() {
+      if ('serial' in navigator) {
+        try {
+          this.port = await navigator.serial.requestPort();
+          await this.port.open({
+            baudRate: 9600
+          });
+          this.status = 'Connected to Flipper in serial mode'
+        } catch {
+          this.status = 'No device selected'
+        }
+      } else {
+        this.status = 'WebSerial API is not supported in your browser'
+      }
+    },
+    async gotoDFU() {
+      // eslint-disable-next-line no-undef
+      const textEncoder = new TextEncoderStream();
+      // eslint-disable-next-line no-unused-vars
+      const writableStreamClosed = textEncoder.readable.pipeTo(this.port.writable);
+      const writer = textEncoder.writable.getWriter();
+      writer.write('dfu\r');
+      writer.close();
+      this.status = 'Rebooted into DFU'
+    },
+    async connectDFU() {
+      // Load the device by WebUSB
+      const selectedDevice = await navigator.usb.requestDevice({ filters: [] });
+      // Create and init the WebDFU instance
+      this.webdfu = new WebDFU(selectedDevice, { forceInterfacesName: true });
+      await this.webdfu.init();
+      if (this.webdfu.interfaces.length == 0) {
+        throw new Error("The selected device does not have any USB DFU interfaces.");
+      }
+      // Connect to first device interface
+      await this.webdfu.connect(0);
+      this.status = 'Connected to Flipper in DFU mode'
+
+      this.getFirmware()
+    },
+    async getFirmware() {
+      try {
+        // Get firmware
+        const buffer = await fetch('https://update.flipperzero.one/fw-0.17.0/f5_full.bin')
+          .then(response => {
+            return response.arrayBuffer()
+          })
+        this.firmwareFile = new Uint8Array(buffer)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async writeFirmware() {
+      // Write firmware in device
+      try {
+        this.status = 'Writing firmware'
+        await this.webdfu.write(1024, this.firmwareFile);
+        this.status = 'OK, reboot Flipper'
+      } catch (error) {
+        console.error(error);
+        this.status = 'Failed to write firmware'
+      }
+    }
+  }
+}
+</script>
