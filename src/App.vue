d062286b625a44dcefb5ff20e4dc0b4e8e85a565
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2021-06-25 04:38:26 +0300

    lint

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default {
     Homepage,
     WebUSB
   },
-  data() {
+  data () {
     return {
       displayHomepage: true,
       userAgent: {
@@ -32,15 +32,15 @@ export default {
       }
     }
   },
-  mounted() {
-    if (navigator.userAgent.indexOf("Chrome") !== -1) this.userAgent.browser = 'Chrome'
-    if (navigator.userAgent.indexOf("YaBrowser") !== -1 || navigator.userAgent.indexOf("Yowser") !== -1) this.userAgent.browser = 'Yandex'
-    if (navigator.userAgent.indexOf("OPR") !== -1) this.userAgent.browser = 'Opera'
-    if (navigator.userAgent.indexOf("Edg") !== -1) this.userAgent.browser = 'Edge'
+  mounted () {
+    if (navigator.userAgent.indexOf('Chrome') !== -1) this.userAgent.browser = 'Chrome'
+    if (navigator.userAgent.indexOf('YaBrowser') !== -1 || navigator.userAgent.indexOf('Yowser') !== -1) this.userAgent.browser = 'Yandex'
+    if (navigator.userAgent.indexOf('OPR') !== -1) this.userAgent.browser = 'Opera'
+    if (navigator.userAgent.indexOf('Edg') !== -1) this.userAgent.browser = 'Edge'
 
-    if (navigator.userAgent.indexOf("Macintosh") !== -1) this.userAgent.os = 'Mac'
-    else if (navigator.userAgent.indexOf("Linux") !== -1) this.userAgent.os = 'Linux'
-    else if (navigator.userAgent.indexOf("Windows") !== -1) this.userAgent.os = 'Windows'
+    if (navigator.userAgent.indexOf('Macintosh') !== -1) this.userAgent.os = 'Mac'
+    else if (navigator.userAgent.indexOf('Linux') !== -1) this.userAgent.os = 'Linux'
+    else if (navigator.userAgent.indexOf('Windows') !== -1) this.userAgent.os = 'Windows'
 
     if (!('serial' in navigator) || !('usb' in navigator)) this.userAgent.browser = 'Not supported'
   }
