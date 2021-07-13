4c8d5d5932d0fd83e03658915a98ca4bf648feee
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2021-07-13 04:15:38 +0300

    fix color codes

--- src/components/WebUSB.vue
+++ src/components/WebUSB.vue
@@ -35,8 +35,8 @@
       <h2>Connected!</h2>
       <div class="card">
         <div class="card-banner">
-          <img v-if="flipper.bodyColor === 'white' || flipper.bodyColor === 'undefined'" src="../assets/flipper-white.png" />
-          <img v-if="flipper.bodyColor === 'black'" src="../assets/flipper-black.png" />
+          <img v-if="flipper.bodyColor === 0 || flipper.bodyColor === 'undefined'" src="../assets/flipper-white.png" />
+          <img v-if="flipper.bodyColor === 1" src="../assets/flipper-black.png" />
         </div>
         <div class="card-desc">
           <pre>
