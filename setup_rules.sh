1cdaa357680a2a82a8d91d8b7a9f9cb7d07a9920
Georgii Surkov <georgii.surkov@outlook.com>
2021-08-10 11:55:37 +0300

    Udev script portability improvements

--- setup_rules.sh
+++ setup_rules.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-RULES_FILE=/usr/lib/udev/rules.d/42-flipperzero.rules
-RULES_TEXT='#Flipper Zero serial port\n
-            SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", ATTRS{manufacturer}=="Flipper Devices Inc.", TAG+="uaccess"\n
-            #Flipper Zero DFU\n
-            SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", ATTRS{manufacturer}=="STMicroelectronics", TAG+="uaccess"\n'
+RULES_DIR=/etc/udev/rules.d
+RULES_FILE=$RULES_DIR/42-flipperzero.rules
+RULES_TEXT='#Flipper Zero serial port
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", ATTRS{manufacturer}=="Flipper Devices Inc.", TAG+="uaccess"
+#Flipper Zero DFU
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", ATTRS{manufacturer}=="STMicroelectronics", TAG+="uaccess"'
 
 warning_message() {
-    echo "You will now be prompted for SUDO password."
+    echo "You will now be asked for SUDO password."
 }
 
 rules_install() {
@@ -15,7 +16,7 @@ rules_install() {
 
     sudo -K &&
     # The danger zone
-    echo -e $RULES_TEXT | sudo tee $RULES_FILE > /dev/null &&
+    echo "$RULES_TEXT" | sudo tee $RULES_FILE > /dev/null &&
     sudo udevadm control --reload-rules &&
     sudo udevadm trigger
     # End of danger zone
@@ -50,12 +51,19 @@ rules_uninstall() {
 }
 
 clear
+
+if [ ! -d $RULES_DIR ]; then
+    echo "Your system seems to have an unusual Udev rules directory, please check your distro's documentation and edit the RULES_DIR variable accordingly."
+    exit
+fi
+
 echo "This script will install system rules that will enable communication with your Flipper Zero."
 
 while true
 do
-    read -p "Choose what to do: [I]nstall, [U]ninstall or [E]xit: " CHOICE
-    case $CHOICE in
+    echo -n "Choose what to do: [I]nstall, [U]ninstall or [E]xit: "
+    read REPLY
+    case $REPLY in
         [Ii]* ) rules_install; break;;
         [Uu]* ) rules_uninstall; break;;
         [Ee]* ) echo "Bye!"; exit;;
