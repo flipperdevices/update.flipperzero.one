eb77997adab7792fe13f1c647dd7c38c2da8436d
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2021-10-12 17:37:06 +0300

    Add resource loader

---
+++ frontend/src/components/updater/resourceLoader.js
@@ -0,0 +1,51 @@
+import untar from 'js-untar'
+import pako from 'pako'
+const inflate = new pako.Inflate()
+
+async function fetchResources (channel, files) {
+  const file = files.find(e => {
+    if (e.type === 'resources_tgz') return e
+    else return undefined
+  })
+  let url = file.url
+  if (channel === 'dev') {
+    url = 'https://update.flipperzero.one/firmware/development/any/resources_tgz'
+  }
+
+  await fetch(url)
+    .then(async response => {
+      const buffer = await response.arrayBuffer()
+      return unpackResources(buffer)
+    })
+}
+
+function unpackResources (buffer) {
+  inflate.push(new Uint8Array(buffer))
+  const ungzipped = inflate.result
+  untar(ungzipped.buffer)
+    .then(extractedFiles => {
+      //
+      console.log(extractedFiles)
+      //
+      parseManifest(extractedFiles.find(e => e.name === 'resources/Manifest').buffer)
+      return extractedFiles
+    })
+}
+
+function parseManifest (manifestFile) {
+  const decoder = new TextDecoder()
+  const manifest = decoder.decode(manifestFile).split('\n')
+  manifest.pop()
+  manifest.forEach(e => {
+    const line = e.split(':')
+    //
+    console.log(line)
+    //
+  })
+}
+
+export {
+  fetchResources,
+  unpackResources,
+  parseManifest
+}
